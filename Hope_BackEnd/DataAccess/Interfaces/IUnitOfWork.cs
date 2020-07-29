549e7db4f4de5ec380e521e8ae60af28e2454a2b
HKDES <samil.ibm@outlok.com>
2020-07-28 19:17:59 -0700

    Add Unit Of Work y scaffold del primer controlador

---
+++ Hope_BackEnd/DataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IBarbaRepository Barba {get;}
+ICabelloRepository Cabello {get;}
+IDenunciaRepository Denuncia {get;}
+IDiccionarioRepository Diccionario {get;}
+IDiscapacidadRepository Discapacidad {get;}
+IInformacionBasicaRepository InformacionBasica {get;}
+IInformacionPoliciaRepository InformacionPolicia {get;}
+IPersonaDenuncianteRepository PersonaDenunciante {get;}
+IPersonaRepository Persona {get;}
+IRopaRepository Ropa {get;}
+ITatuajeRepository Tatuaje {get;}
+ITelefonoRepository Telefono {get;}
+        int Complete();
+    }
+}
---
+++ Hope_BackEnd/DataAccess/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.EntityFramework;
+using DataAccess.Interfaces;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class UnitOfWork
+    {
+        private readonly HopeContext _context;
+        public UnitOfWork(HopeContext context)
+        {
+            _context = context;
+            Barba = new BarbaRepository(_context);
+        Cabello = new CabelloRepository(_context);
+        Denuncia = new DenunciaRepository(_context);
+        Diccionario = new DiccionarioRepository(_context);
+        Discapacidad = new DiscapacidadRepository(_context);
+        InformacionBasica = new InformacionBasicaRepository(_context);
+        InformacionPolicia = new InformacionPoliciaRepository(_context);
+        PersonaDenunciante = new PersonaDenuncianteRepository(_context);
+        Persona = new PersonaRepository(_context);
+        Ropa = new RopaRepository(_context);
+        Tatuaje = new TatuajeRepository(_context);
+        Telefono = new TelefonoRepository(_context);
+    }
+        public IBarbaRepository Barba {get; private set;}
+        public ICabelloRepository Cabello {get; private set;}
+        public IDenunciaRepository Denuncia {get; private set;}
+        public IDiccionarioRepository Diccionario {get; private set;}
+        public IDiscapacidadRepository Discapacidad {get; private set;}
+        public IInformacionBasicaRepository InformacionBasica {get; private set;}
+        public IInformacionPoliciaRepository InformacionPolicia {get; private set;}
+        public IPersonaDenuncianteRepository PersonaDenunciante {get; private set;}
+        public IPersonaRepository Persona {get; private set;}
+        public IRopaRepository Ropa {get; private set;}
+        public ITatuajeRepository Tatuaje {get; private set;}
+        public ITelefonoRepository Telefono {get; private set;}
+
+    }
+}
