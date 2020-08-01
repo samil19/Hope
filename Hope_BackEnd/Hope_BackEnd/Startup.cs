c50e2381749e81eb830393fe77ebad3ca5e3e425
HKDES <samil.ibm@outlok.com>
2020-07-31 22:32:58 -0700

    Add Swagger

--- Hope_BackEnd/Hope_BackEnd/Startup.cs
+++ Hope_BackEnd/Hope_BackEnd/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Root;
 
 namespace Hope_BackEnd
@@ -26,6 +27,10 @@ namespace Hope_BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Hope API" });
+            });
             CompositionRoot.InjectDependencies(services);
             services.AddControllers();
         }
@@ -37,6 +42,12 @@ namespace Hope_BackEnd
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hope API");
+            });
 
             app.UseHttpsRedirection();
 
